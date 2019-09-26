3e94192206f8303d73f6fc0831fdba86c1cf4aaa
Víctor Sisqués Cortés <victor@localhost.localdomain>
2019-09-26 18:29:28 +0200

    punto y direccion acabados

--- bibliotecas/main.cpp
+++ bibliotecas/main.cpp
@@ -1,6 +1,72 @@
-#include "punto&direccion.h"
+#include "punto_direccion.h"
+#include <iostream>
+using namespace std;
+
 
 int main(){
+	point p = newPoint(0,1.0,1.0);
+	point q = newPoint(0,2.0,3.0);
+	point r = newPoint(0,3.0,2.0);
+	cout << "puntos" << endl;
+	cout << p.x << " " << p.y << " " << p.z << endl; 
+	cout << q.x << " " << q.y << " " << q.z << endl; 
+	cout << r.x << " " << r.y << " " << r.z << endl; 
+
+	dir d1 = newDir(0,1.0,4.0);
+	dir d2 = q - p;
+	dir d3 = d1 + d2; 
+	dir d4 = (r-q) - d3;
+
+	dir d5 = 3.1*d1;
+	dir d6 = 3.1/d5;
 	
+	cout << "direcciones" << endl;
+	cout << d1.x << " " << d1.y << " " << d1.z << endl; 
+	cout << d2.x << " " << d2.y << " " << d2.z << endl; 
+	cout << d3.x << " " << d3.y << " " << d3.z << endl;
+	cout << d4.x << " " << d4.y << " " << d4.z << endl;
+	cout << d5.x << " " << d5.y << " " << d5.z << endl;
+	cout << d6.x << " " << d6.y << " " << d6.z << endl;
+
+
+	cout << "modulo de d5" << endl; 
+	cout << mod(d5) << endl;
+
+	point s = p + d1;
+
+	cout << "punto s" << endl;
+	cout << s.x << " " << s.y << " " << s.z << endl; 
+
+
+	cout << "calculo de producto escalar" << endl;
+
+	dir a = newDir(0,1.0,1.0);
+	dir b = newDir (0,5.0,0);
+
+	cout << a.x << " " << a.y << " " << a.z << endl; 
+	cout << b.x << " " << b.y << " " << b.z << endl;
+
+	double escalar = dot(a,b);
+	double angulo = angle(a,b);
 	
+	cout << escalar << endl;
+	cout << angulo  << endl;
+
+
+	cout << "calculo de producto vectorial" << endl;
+;
+
+	cout << a.x << " " << a.y << " " << a.z << endl; 
+	cout << b.x << " " << b.y << " " << b.z << endl;
+
+	dir vectorial = cross(a,b);
+	
+	cout << vectorial.x << " " << vectorial.y << " " << vectorial.z << endl;
+	double angulo_ab = angle(a,b);
+	double angulo_bvect = angle(b,vectorial);
+	double angulo_vecta = angle(vectorial,a);
+	cout << angulo_ab << endl; 
+	cout << angulo_bvect << endl; 
+	cout << angulo_vecta << endl; 
+
 }
---
+++ bibliotecas/punto_direccion.h
@@ -0,0 +1,87 @@
+#ifndef _PUNTO_DIRECCION_H_
+#define _PUNTO_DIRECCION_H_
+
+#include <cmath>
+
+struct point {
+	public:
+		double x;
+		double y;
+		double z;
+		double w;
+};
+
+struct dir {
+  public:
+    	double x;
+		double y;
+		double z;
+		double w;
+};
+
+	point newPoint(double _x, double _y, double _z) {
+		point p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+        p.w = 1;
+		return p;
+	}
+
+	dir newDir(double _x, double _y, double _z) {
+		dir p;
+		p.x = _x;
+		p.y = _y;
+		p.z = _z;
+        p.w = 0;
+		return p;
+	}
+
+
+	point operator +(point p, point q)         {     
+		return newPoint(p.x + q.x, p.y + q.y, p.z + q.z);     
+	}   
+	 
+	dir operator -(point p, point q)         {     
+		return newDir(p.x - q.x, p.y - q.y, p.z - q.z);   
+	} 
+
+	point operator +(point p, dir q)         {     
+		return newPoint(p.x + q.x, p.y + q.y, p.z + q.z);     
+	}   
+
+	dir operator +(dir p, dir q)         {     
+		return newDir(p.x + q.x, p.y + q.y, p.z + q.z);     
+	}   
+
+	dir operator -(dir p, dir q)         {     
+		return newDir(p.x - q.x, p.y - q.y, p.z - q.z);     
+	}   
+
+	dir operator *( double q, dir p)         {     
+		return newDir(p.x*q, p.y*q, p.z*q);     
+	}   
+
+	dir operator /(double q, dir p)         {     
+		return newDir(p.x/q, p.y/q, p.z/q);     
+	}   
+
+	double mod(dir d){
+		return sqrt( pow(d.x,2) + pow(d.y,2) + pow(d.z,2) );
+	}
+	
+	double dot(dir d, dir e) {
+		return d.x*e.x + d.y*e.y + d.z*e.z;
+	}
+	
+	double angle(dir d, dir e) {
+		return acos( ( dot(d,e) )/( mod(d)*mod(e) ) );
+	}
+	   
+	   
+	dir cross(dir d, dir e) {
+		return newDir(d.y*e.z - d.z*e.y, d.z*e.x - d.x*e.z, d.x*e.y -d.y*e.x);
+	}
+   
+
+#endif
