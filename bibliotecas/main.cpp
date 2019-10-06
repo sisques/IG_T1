ab2e062182b949739718b2939011cd85f0d0496b
karny1998 <45491071+karny1998@users.noreply.github.com>
2019-10-06 19:37:29 +0200

    Add files via upload

--- bibliotecas/main.cpp
+++ bibliotecas/main.cpp
@@ -3,6 +3,8 @@
 #include "plano.h"
 #include <iostream>
 #include "math.h"
+#include <tranformations.h>
+
 using namespace std;
 
 
@@ -14,25 +16,28 @@ int main(){
 	bool ok;
 	point centro1, centro2;
 	point ciudad1, ciudad2;
+	point estacion1, estacion2;
+	point meridianRef1, meridianRef2;
+	dir ejeX1, ejeY1, ejeZ1, ejeX2, ejeY2, ejeZ2;
 	dir eje1, eje2;
 	
-	//----------PLANETA 1----------//
+	//----------PLANETA EMISOR----------//
 
 	// Primero se le solicita al usuario las coordenadas en UCS del centro, eje 
-	// y cuidad de referencia del planeta 1
-	cout << "Introduce las coordenadas del centro del planeta 1 en UCS" << endl;
+	// y cuidad de referencia del planeta emisor
+	cout << endl << "Introduce las coordenadas del centro del planeta emisor en UCS" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
 	cout << "z: " ;cin >> z;
 	centro1 = newPoint(x,y,z);
 
-	cout << "Introduce la direccion del eje del planeta 1 en UCS" << endl;
+	cout << endl << "Introduce la direccion del eje del planeta emisor en UCS" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
 	cout << "z: " ;cin >> z;
 	eje1 = newDir(x,y,z);
 
-	cout << "Introduce las coordenadas de la ciudad de referencia del planeta 1 en UCS" << endl;
+	cout << endl << "Introduce las coordenadas de la ciudad de referencia del planeta emisor en UCS" << endl;
 	cout << "Las coordenadas de la ciudad de referencia se usarán para definir el 0-meridiano" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
@@ -45,59 +50,65 @@ int main(){
 	r1 = mod(eje1)/2;
 	r2 = dist(centro1, ciudad1);
 
-	ok = abs(r1-r2) <= 0.000001 ? true : false ;
+	ok = abs(r1-r2) <= 1;//0.000001 ? true : false ;
 
 	if (ok) {
-		cout << "Los valores introducidos son correctos." << endl;
+		cout << endl << "Los valores introducidos son correctos." << endl;
 	} else {
-		cout << "Valores incorrectos, se va a detener la ejecución." << endl;
+		cout << endl << "Valores incorrectos, se va a detener la ejecución." << endl;
 		exit(0);
 	}
 
 
 	// Acto seguido se pasará a solicitar al usuario la inclinación y azimut que definen la estacion
-	// planetaria del planeta 1
-	cout << "Introduce la inclinación de la estación planetaria del planeta 1, rango (0,pi)" << endl;
+	// planetaria del planeta emisor
+	cout << endl << "Introduce la inclinación de la estación planetaria del planeta emisor, rango (0,pi)" << endl;
 	cout << "La inclinación es el angulo de la estación respecto al eje del planeta" << endl;
 	cout << "Inclinación: " ;cin >> incl1;
 
 	if (!(incl1 > 0 && incl1 < M_PI)) {
-		cout << "La inclinación introducida no es valida, se va a detener la ejecución." << endl;
+		cout << endl << "La inclinación introducida no es valida, se va a detener la ejecución." << endl;
 		exit(1);
 	}
 	
-	cout << "Introduce el azimut de la estación planetaria del planeta 1, rango (-pi, pi]" << endl;
+	cout << endl << "Introduce el azimut de la estación planetaria del planeta emisor, rango (-pi, pi]" << endl;
 	cout << "El azimut es el angulo de la estación espacial respecto del 0-meridiano" << endl;
 	cout << "Azimut: " ;cin >> azim1;
 
 	if (!(azim1 > -M_PI && azim1 <= M_PI)) {
-		cout << "El azimut introducida no es valida, se va a detener la ejecución." << endl;
+		cout << endl << "El azimut introducida no es valida, se va a detener la ejecución." << endl;
 		exit(1);
 	}
 
+	//Cálculo de la estación 1
+	estacion1 = sphereParametric(centro1,r1, incl1, azim1);
+	//meridianRef1 = sphereParametric(centro2,r1, 0, 0);
+	ejeY1  = 2 / eje1;
+	if(ejeY1.y < 0){ejeY1  = -1 * eje1;}
+	ejeZ1 = cross(ejeY1, (ciudad1 - centro1));
+	ejeX1 = cross(ejeY1, ejeZ1);
+	//ejeX1 = meridianRef1 - centro1;
+	//ejeZ1 = cross(ejeX1, ejeY1);
+	estacion1 = desbasec(ejeX1,ejeY1,ejeZ1,centro1, estacion1);
 
-	// Finalmente se calculara la posición de la estación en el sistema de coordenadas del planeta
-	//TODO
-
-
-	//----------PLANETA 2----------//
+	//----------PLANETA RECEPTOR----------//
 
 
 	// Luego se le solicita al usuario las coordenadas en UCS del centro, eje 
-	// y cuidad de referencia del planeta 2
-	cout << "Introduce las coordenadas del centro del planeta 2 en UCS" << endl;
+	// y cuidad de referencia del planeta receptor
+	cout << "Introduce las coordenadas del centro del planeta receptor en UCS" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
 	cout << "z: " ;cin >> z;
 	centro2 = newPoint(x,y,z);
 
-	cout << "Introduce la direccion del eje del planeta 2 en UCS" << endl;
+	cout << endl << "Introduce la direccion del eje del planeta receptor en UCS" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
 	cout << "z: " ;cin >> z;
 	eje2 = newDir(x,y,z);
 
-	cout << "Introduce las coordenadas de la ciudad de referencia del planeta 2 en UCS" << endl;
+	cout << endl << "Introduce las coordenadas de la ciudad de referencia del planeta receptor en UCS" << endl;
 	cout << "Las coordenadas de la ciudad de referencia se usarán para definir el 0-meridiano" << endl;
 	cout << "x: " ;cin >> x;
 	cout << "y: " ;cin >> y;
@@ -110,42 +121,55 @@ int main(){
 	r1 = mod(eje2)/2;
 	r2 = dist(centro2, ciudad2);
 
-	ok = abs(r1-r2) <= 0.000001 ? true : false ;
+	ok = abs(r1-r2) <= 1;//0.000001 ? true : false ;
 
 	if (ok) {
-		cout << "Los valores introducidos son correctos." << endl;
+		cout << endl << "Los valores introducidos son correctos." << endl;
 	} else {
-		cout << "Valores incorrectos, se va a detener la ejecución." << endl;
+		cout << endl << "Valores incorrectos, se va a detener la ejecución." << endl;
 		exit(0);
 	}
 
 
 	// Acto seguido se pasará a solicitar al usuario la inclinación y azimut que definen la estacion
-	// planetaria del planeta 2
-	cout << "Introduce la inclinación de la estación planetaria del planeta 2, rango (0,pi)" << endl;
+	// planetaria del planeta receptor
+	cout << endl << "Introduce la inclinación de la estación planetaria del planeta receptor, rango (0,pi)" << endl;
 	cout << "La inclinación es el angulo de la estación respecto al eje del planeta" << endl;
-	cout << "Inclinación: " ;cin >> incl1;
+	cout << "Inclinación: " ;cin >> incl2;
 
 	if (!(incl2 > 0 && incl2 < M_PI)) {
-		cout << "La inclinación introducida no es valida, se va a detener la ejecución." << endl;
+		cout << endl << "La inclinación introducida no es valida, se va a detener la ejecución." << endl;
 		exit(1);
 	}
 
-	cout << "Introduce el azimut de la estación planetaria del planeta 2, rango (-pi, pi]" << endl;
+	cout << endl << "Introduce el azimut de la estación planetaria del planeta receptor, rango (-pi, pi]" << endl;
 	cout << "El azimut es el angulo de la estación espacial respecto del 0-meridiano" << endl;
 	cout << "Azimut: " ;cin >> azim2;
 
 	if (!(azim2 > -M_PI && azim2 <= M_PI)) {
-		cout << "El azimut introducido no es valido, se va a detener la ejecución." << endl;
+		cout << endl << "El azimut introducido no es valido, se va a detener la ejecución." << endl;
 		exit(1);
 	}
 
-	// Finalmente se calculara la posición de la estación en el sistema de coordenadas del planeta
-	//TODO
+	//Cálculo de la estación 2
+	estacion2 = sphereParametric(centro2,r1, incl2, azim2);
+	//meridianRef2 = sphereParametric(centro2,r1, 0, 0);
+	ejeY2  = 2 / eje2;
+	if(ejeY2.y < 0){ejeY2  = -1 * eje2;}
+	ejeZ2 = cross(ejeY2, (ciudad2 - centro2));
+	ejeX2 = cross(ejeY2, ejeZ2);
+	///ejeX2 = meridianRef2 - centro2;
+	//ejeZ2 = cross(ejeX2, ejeY2);
+	estacion2 = desbasec(ejeX2,ejeY2,ejeZ2,centro2, estacion2);
 
 
 	//----------CÁLCULO DE LA CONEXIÓN----------//
+	dir conex = estacion2 - estacion1;
+	dir d1 = basec(ejeX1, ejeY1, ejeZ1, centro1, conex);
+	dir d2 = -1 * basec(ejeX2, ejeY2, ejeZ2, centro2, conex);
+	
+	cout << endl << "La estación emisora lanzará en la dirección x: " << d1.x << " y: " << d1.y << " z: " << d1.z << endl;
+	if(d1.y < 0){cout << "Problema en la dirección de emisión. ALERTA" << endl;}
+	cout << endl << "La estación receptora recibira en la dirección x: " << d2.x << " y: " << d2.y << " z: " << d2.z << endl;
+	if(d2.y < 0){cout << "Problema en la dirección de recepción. ALERTA" << endl;}
 }
-
-
-
--- bibliotecas/tranformations.h
+++ bibliotecas/tranformations.h
@@ -30,6 +30,88 @@ using namespace std;
 		return newDir(a, b, c);
 	}
 	
+		double det3x3(const double m[3][3]){
+		return m[0][0]*m[1][1]*m[2][2] + m[0][1]*m[1][2]*m[2][0] + m[1][0]*m[2][1]*m[0][2]
+			   - m[0][2]*m[1][1]*m[2][0] - m[1][2]*m[2][1]*m[0][0] - m[1][0]*m[0][1]*m[2][2];
+	}
+	
+	double det4x4(const double m[4][4]){
+		double det = 0.0;
+		if(m[0][0] != 0){
+			double aux[3][3] = { {m[1][1], m[1][2], m[1][3]},
+						{m[2][1], m[2][2], m[2][3]},
+						{m[3][1], m[3][2], m[3][3]}
+						};
+			det += m[0][0]*det3x3(aux);
+		}
+		if(m[1][0] != 0){
+			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
+						{m[2][1], m[2][2], m[2][3]},
+						{m[3][1], m[3][2], m[3][3]}
+						};
+			det -= m[1][0]*det3x3(aux);
+		}
+		if(m[2][0] != 0){
+			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
+						{m[1][1], m[1][2], m[1][3]},
+						{m[3][1], m[3][2], m[3][3]}
+						};
+			det += m[2][0]*det3x3(aux);
+		}
+		if(m[3][0] != 0){
+			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
+						{m[1][1], m[1][2], m[1][3]},
+						{m[2][1], m[2][2], m[2][3]}
+						};
+			det -= m[3][0]*det3x3(aux);
+		}
+		
+		return det;
+	}
+	
+	void adj(const double m[4][4], double adj[4][4]){
+		for(int i = 0; i < 4; ++i){
+			for(int j = 0; j < 4; ++j){
+				double aux[3][3];
+				int h = 0, k = 0;
+				for(int x = 0; x < 4; ++x){
+					for(int y = 0; y < 4 && h <3 && k < 3; ++y){
+						if(y != j){
+							aux[h][k] = m[x][y];
+							++k;
+						}
+					}
+					if(x != i){++h;}
+					k = 0;
+				}
+				adj[i][j] = pow(-1.0, i+j)*det3x3(aux);
+			}
+		}
+	}
+	
+	void trasp(const double m[4][4], double adj[4][4]){
+		for(int i = 0; i < 4; ++i){
+			for(int j = 0; j < 4; ++j){
+				adj[i][j] = m[j][i];
+			}
+		}
+	}
+	
+	void div(const double m[4][4], double x, double d[4][4]){
+		for(int i = 0; i < 4; ++i){
+			for(int j = 0; j < 4; ++j){
+				d[i][j] = m[i][j]/x;
+			}
+		}
+	}
+	
+	void inver(const double m[4][4], double inv[4][4]){
+		double aux[4][4], aux2[4][4];
+		adj(m,aux);
+		trasp(aux, aux2);
+		div(aux2, det4x4(m), inv);
+	}
+	
 	point trans(const point p, const double x, const double y, const double z){
 		double m[4][4] = { {1, 0, 0, x},
 						   {0, 1, 0, y},
@@ -210,88 +292,6 @@ using namespace std;
 		double aux[4][4];
 		inver(m,aux);
 		return aux * d;
-	}
-	
-	double det3x3(const double m[3][3]){
-		return m[0][0]*m[1][1]*m[2][2] + m[0][1]*m[1][2]*m[2][0] + m[1][0]*m[2][1]*m[0][2]
-			   - m[0][2]*m[1][1]*m[2][0] - m[1][2]*m[2][1]*m[0][0] - m[1][0]*m[0][1]*m[2][2];
-	}
-	
-	double det4x4(const double m[4][4]){
-		double det = 0.0;
-		if(m[0][0] != 0){
-			double aux[3][3] = { {m[1][1], m[1][2], m[1][3]},
-						{m[2][1], m[2][2], m[2][3]},
-						{m[3][1], m[3][2], m[3][3]}
-						};
-			det += m[0][0]*det3x3(aux);
-		}
-		if(m[1][0] != 0){
-			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
-						{m[2][1], m[2][2], m[2][3]},
-						{m[3][1], m[3][2], m[3][3]}
-						};
-			det -= m[1][0]*det3x3(aux);
-		}
-		if(m[2][0] != 0){
-			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
-						{m[1][1], m[1][2], m[1][3]},
-						{m[3][1], m[3][2], m[3][3]}
-						};
-			det += m[2][0]*det3x3(aux);
-		}
-		if(m[3][0] != 0){
-			double aux[3][3] = { {m[0][1], m[0][2], m[0][3]},
-						{m[1][1], m[1][2], m[1][3]},
-						{m[2][1], m[2][2], m[2][3]}
-						};
-			det -= m[3][0]*det3x3(aux);
-		}
-		
-		return det;
-	}
-	
-	void adj(const double m[4][4], double adj[4][4]){
-		for(int i = 0; i < 4; ++i){
-			for(int j = 0; j < 4; ++j){
-				double aux[3][3];
-				int h = 0, k = 0;
-				for(int x = 0; x < 4; ++x){
-					for(int y = 0; y < 4 && h <3 && k < 3; ++y){
-						if(y != j){
-							aux[h][k] = m[x][y];
-							++k;
-						}
-					}
-					if(x != i){++h;}
-					k = 0;
-				}
-				adj[i][j] = pow(-1.0, i+j)*det3x3(aux);
-			}
-		}
-	}
-	
-	void trasp(const double m[4][4], double adj[4][4]){
-		for(int i = 0; i < 4; ++i){
-			for(int j = 0; j < 4; ++j){
-				adj[i][j] = m[j][i];
-			}
-		}
-	}
-	
-	void div(const double m[4][4], double x, double d[4][4]){
-		for(int i = 0; i < 4; ++i){
-			for(int j = 0; j < 4; ++j){
-				d[i][j] = m[i][j]/x;
-			}
-		}
-	}
-	
-	void inver(const double m[4][4], double inv[4][4]){
-		double aux[4][4], aux2[4][4];
-		adj(m,aux);
-		trasp(aux, aux2);
-		div(aux2, det4x4(m), inv);
 	}
 	
 	#endif
