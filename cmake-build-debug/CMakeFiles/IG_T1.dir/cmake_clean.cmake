418a8182be1040412ef3ab503b924b96e523863e
sisques <sisques>
2019-11-26 18:30:06 +0100

    NuevoProyecto

---
+++ cmake-build-debug/CMakeFiles/IG_T1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IG_T1.dir/IG_T3/main.cpp.o"
+  "IG_T1"
+  "IG_T1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IG_T1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
