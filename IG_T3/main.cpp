e2f97ba623f685c8e7f3e8e1e8bf3fbb9b2401c2
karny1998 <45491071+karny1998@users.noreply.github.com>
2019-11-26 21:49:00 +0100

    Add files via upload

--- IG_T3/main.cpp
+++ IG_T3/main.cpp
@@ -1,11 +1,10 @@
 #include "bibliotecas/punto_direccion.h"
 #include "bibliotecas/camara.h"
 #include "bibliotecas/figuras.h"
-#include "/bibliotecas/matrix.h"
+#include "bibliotecas/matrix.h"
 #include "bibliotecas/monteCarlo.h"
 #include "bibliotecas/ply_reader.h"
 #include "bibliotecas/perlinNoise.h"
-#include "bibliotecas/ppm_reader.h"
 
 #include <iostream>
 #include <string>
@@ -19,64 +18,81 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <iomanip>
 
 using namespace std;
 
 camara c;
 
+
 list<shared_ptr<figura>> setUpScene(){
-    Matrix m =  translate(0,0,0);
-    string file = "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\test.ply";
-    list<shared_ptr<figura>> elementos = plyReader(file,c, 3, m);
+    string file = "/home/victor/4o/IG/IG_T1/models/human.ply";
+    //int matrixSize = 3;
+    double brdfValues[] = {0.35,0.35};
+	event_enum eventos[] = {REFRACTION, REFLEXION};
+    //Matrix m[matrixSize] ={rotateX(-M_PI/2), rotateY(M_PI/4 + M_PI/2), translate(0,-0.3,0.5)};
+    materialProperties mp = materialProperties(false, false, eventos, brdfValues);
+    list<shared_ptr<figura>> elementos; //= plyReader(file,&mp, m, matrixSize);
+
+	mp.setRGB(120, 120, 120);
+    shared_ptr<figura> fondo = make_shared<plano>(plano(newPoint(0,0,1), newDir(0,0,-1), mp));
+    shared_ptr<figura> suelo = make_shared<plano>(plano(newPoint(0,-0.5,0), newDir(0,1,0),mp));
+    shared_ptr<figura> techo = make_shared<plano>(plano(newPoint(0,0.5,0), newDir(0,-1,0),mp));
+    mp.setRGB(255, 0, 0);
+    shared_ptr<figura> izquierda = make_shared<plano>(plano(newPoint(-0.5,0,0), newDir(1,0,0),mp));
+    mp.setRGB(0, 255, 0);
+    shared_ptr<figura> derecha = make_shared<plano>(plano(newPoint(0.5,0,0), newDir(-1,0,0),mp));
+
+    materialProperties phong = materialProperties(false,0,0,255, false, eventos, brdfValues);
+    shared_ptr<figura> esferaPhong = make_shared<esfera>(newPoint(0.25,-0.25,0.5), 0.25, phong);
 
 
-    shared_ptr<figura> p1 = make_shared<esfera>(esfera(c, newPoint(-50,0,100), 25, 255,100, 100,5));
-    shared_ptr<figura> p2 = make_shared<esfera>(esfera(c, newPoint(50,0,100), 25, 100,255, 100,5));
-    shared_ptr<figura> p3 = make_shared<esfera>(esfera(c, newPoint(0,50,100), 25, 51,153, 255,4));
-    shared_ptr<figura> p4 = make_shared<esfera>(esfera(c, newPoint(0,-50,100), 25, 255,100, 255,4));
-
-
-    shared_ptr<figura> fondo = make_shared<plano>(plano(c, newPoint(0,0,401), newDir(0,0,-1), 0,0, 0, 8, "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\agua.ppm"));
-	shared_ptr<figura> fondo3 = make_shared<plano>(plano(c, newPoint(-250,0,500), newDir(1,0,0), 0,0, 0, 8, "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\arena.ppm"));
-	shared_ptr<figura> fondo4 = make_shared<plano>(plano(c, newPoint(250,0,500), newDir(-1,0,0), 0,0, 0, 8, "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\arena.ppm"));
-	shared_ptr<figura> fondo5 = make_shared<plano>(plano(c, newPoint(0,-250,500), newDir(0,1,0), 0,0, 0, 8, "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\arena.ppm"));
-	shared_ptr<figura> fondo6 = make_shared<plano>(plano(c, newPoint(0,250,500), newDir(0,-1,0), 0,0, 0, 8, "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\arena.ppm"));
-	shared_ptr<figura> fondo2 = make_shared<plano>(plano(c, newPoint(0,0,400), newDir(0,0,-1), 51,153, 250, 7));
-    elementos.push_back(p1);
-    elementos.push_back(p2);
-    elementos.push_back(p3);
-    elementos.push_back(p4);
-    elementos.push_back(fondo);
-	elementos.push_back(fondo2);
-	elementos.push_back(fondo3);
-	elementos.push_back(fondo4);
-	elementos.push_back(fondo5);
-	elementos.push_back(fondo6);
+    materialProperties especular_refracion = materialProperties(false,0,255,255, false, eventos, brdfValues);
+    shared_ptr<figura> esferaEspecularRefracion = make_shared<esfera>(newPoint(-0.2,-0.2,0.5), 0.2, especular_refracion);
 	
+	materialProperties light = materialProperties(true,255,255,255, false, eventos, brdfValues);
+    point p1 = newPoint(-0.25,0.49,0.25);
+    point p2 = newPoint(0.25,0.49,0.25);
+    point p3 = newPoint(-0.25,0.49,0.75);
+    point p4 = newPoint(0.25,0.49,0.75);
+    shared_ptr<figura> lght_src_1 = make_shared<triangulo>(triangulo(p1,p2,p3,light));
+    shared_ptr<figura> lght_src_2 = make_shared<triangulo>(triangulo(p2,p3,p4,light));
 
-
-
+    elementos.push_back(lght_src_1);
+    elementos.push_back(lght_src_2);
+	
+    elementos.push_back(fondo);
+    elementos.push_back(suelo);
+    elementos.push_back(techo);
+    elementos.push_back(izquierda);
+    elementos.push_back(derecha);
+    elementos.push_back(esferaPhong);
+    elementos.push_back(esferaEspecularRefracion);
     return elementos;
 }
 
+int lineasCompletadas = 0;
+
 void generateScene( monteCarlo mc, const list<shared_ptr<figura>> e,
-					const string fOut, const int hMin, const int hMax, const int w){
+					const string fOut, const int hMin, const int hMax, const int w, const int h){
 	int R,G,B;
 	fstream flujoOut;
 	flujoOut.open((fOut).c_str(), ios::out);
-	texture4 tex;
 	for (int i = hMin; i <= hMax; ++i){
         for (int j = 0; j < w; j++){
             mc.rtx(e,i,j,R,G,B);
             flujoOut << R << " " << G << " " << B;
             flujoOut << "	";
         }
+        lineasCompletadas++;
+        double prog = (double)lineasCompletadas / (double)h;
+		cout << "\r" << "progreso: " << fixed << setprecision(2) << setw(6) << prog*100 << "%" << flush;
     }
 	flujoOut.close();
 }
 
 int main(){
-	/*
+    /*
      * camara       x   y   z
      *      o   =   0   0   0
      *      l   =   1   0   0
@@ -94,7 +110,9 @@ int main(){
 	c =  newCamara(origenCamara, alturaPlano, anchuraPlano, distanciaPlano);
     double rpp = 0, w = 0, h = 0;
     string fOut = "";
+
     /*cout << "Introduce la altura de la imagen deseada:" << endl;
+     *
     cin >> h;
 
     cout << "Introduce la anchura de la imagen deseada:" << endl;
@@ -108,17 +126,22 @@ int main(){
     */
     h = 1024;
     w = 1024;
-    rpp = 1;
+    rpp = 10;
+    int threads = 10;
+    if (threads > h || threads > w){
+        cerr << "Numero de threads incompatible con la resolucion de la imagen" << endl;
+        exit(5);
+    }
     fOut = "test";
     string ruta = "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\";
-   int threads = 20;
+
 	list<shared_ptr<figura>> e = setUpScene();
 	thread th[threads];
 	int hMin = 0, hMax = - 1 + h/threads;
 	for(int i = 0; i < threads;++i){
-		monteCarlo mc(c,h,w,10);
+		monteCarlo mc(c,h,w,rpp);
 		if(i == threads-1){hMax = h - 1;}
-		th[i] = thread(&generateScene, mc, e, ruta+to_string(i), hMin, hMax, w);
+		th[i] = thread(&generateScene, mc, e, ruta+to_string(i), hMin, hMax, w, h);
 		hMin += h/threads;
 		hMax += h/threads;
 	}
