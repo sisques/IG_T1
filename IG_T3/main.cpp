12b04004003b3a47e7889e9be6aa9af40e6adf9f
karny1998 <45491071+karny1998@users.noreply.github.com>
2020-02-01 22:43:28 +0100

    Add files via upload

--- IG_T3/main.cpp
+++ IG_T3/main.cpp
@@ -22,13 +22,53 @@ using namespace std;
 
 camara c;
 
-list<shared_ptr<figura>> setUpScene(){
+list<shared_ptr<figura>> set_room(){
 
+    list<shared_ptr<figura>> elementos;
+	
+	double phong[] =        {0.90,0.0,0.0};
+    event_enum eventos[] =  {PHONG, REFRACTION, REFLEXION };
+
+    materialProperties floor = materialProperties(false, eventos, phong);
+    floor.setKdPhong(216,216,216);
+    floor.setKsPhong(216,216,216);
+	
+    materialProperties left = materialProperties(false, eventos, phong);
+    left.setKdPhong(216,21,21);
+    left.setKsPhong(216,21,21);
+
+    materialProperties right = materialProperties(false, eventos, phong);
+    right.setKdPhong(21,216,21);
+    right.setKsPhong(21,216,21);
+
+    materialProperties luz = materialProperties(true, eventos, phong);
+    luz.setKs(255,255,255);
+    luz.setAlfa(0);
+	
+    shared_ptr<figura> floor_ = make_shared<plano>(plano(newPoint(0,0,1), newDir(0,0,-1),floor));
+    shared_ptr<figura> ceil_ = make_shared<plano>(plano(newPoint(0,0.5,0), newDir(0,-1,0),luz));
+    shared_ptr<figura> back_ = make_shared<plano>(plano(newPoint(0,-0.5,0), newDir(0,1,0),floor));
+    shared_ptr<figura> left_ = make_shared<plano>(plano(newPoint(-0.5,0,0), newDir(1,0,0),left));
+    shared_ptr<figura> right_ = make_shared<plano>(plano(newPoint(0.5,0,0), newDir(-1,0,0),right));
+
+    shared_ptr<figura> puntoLuz_1 = make_shared<punto>(punto(newPoint(0,0.49,0.5), luz));
+    //elementos.push_back(puntoLuz_1);
+    elementos.push_back(floor_);
+    elementos.push_back(ceil_);
+    elementos.push_back(back_);
+    elementos.push_back(left_);
+    elementos.push_back(right_);
+
+    return elementos;
+}
+
+// Devuelve una escena medianamente compleja
+list<shared_ptr<figura>> setUpSceneCool(){
+	// Definición de materiales
     double reflection[] = {0,0.0,0.9};
     double refraction[] = {0.0,0.9,0.0};
     double wall[] = {0.90,0.0,0.0};
 	double l[] = {1.0};
-	double pelota[] = {0.20,0.0,0.7};
     event_enum eventos[] = {PHONG, REFRACTION, REFLEXION };
 	event_enum eventos2[] = {EMISSION};
     materialProperties mp = materialProperties(false, eventos, wall);
@@ -48,36 +88,26 @@ list<shared_ptr<figura>> setUpScene(){
 	refraccion.setKdPhong(0,255,255);
 	refraccion.setKsPhong(0,255,255);
 
-
+	// Definición de geometrías
     list<shared_ptr<figura>> elementos;
 	shared_ptr<figura> puntoLuz = make_shared<punto>(punto(newPoint(0,0,0.95), light));
-	shared_ptr<figura> puntoLuz2 = make_shared<punto>(punto(newPoint(0,-0.4,0.5), light));
 	//mp.setKd(120, 120, 120);
 	//mp.setKs(120, 120, 120);
 	mp.setKdPhong(170, 170, 170);
 	mp.setKsPhong(120, 120, 120);
-    //shared_ptr<figura> fondo = make_shared<plano>(plano(newPoint(0,0,1), newDir(0,0,-1),IMAGE,"C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\aragon.ppm", mp));
 	shared_ptr<figura> fondo = make_shared<plano>(plano(newPoint(0,0,1), newDir(0,0,-1), mp));
-	//fondo->setScale(400);
-    //shared_ptr<figura> suelo = make_shared<plano>(plano(newPoint(0,-0.5,0), newDir(0,1,0),IMAGE,"C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\aragon.ppm",mp));
 	shared_ptr<figura> suelo = make_shared<plano>(plano(newPoint(0,-0.5,0), newDir(0,1,0),mp));
-	//suelo->setScale(400);
-    shared_ptr<figura> techo = make_shared<plano>(plano(newPoint(0,0.5,0), newDir(0,-1,0),light));
+    shared_ptr<figura> techo = make_shared<plano>(plano(newPoint(0,0.5,0), newDir(0,-1,0),mp));
     //mp.setKd(255,0,0);
 	//mp.setKs(255,0,0);
 	mp.setKdPhong(255,0,0);
 	mp.setKsPhong(120,0,0);
-	//shared_ptr<figura> izquierda = make_shared<plano>(plano(newPoint(-0.5,0,0), newDir(1,0,0),IMAGE,"C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\aragon.ppm", mp));
-	//izquierda->setScale(400);
     shared_ptr<figura> izquierda = make_shared<plano>(plano(newPoint(-0.5,0,0), newDir(1,0,0), mp));
     //mp.setKd(0,255,0);
 	//mp.setKs(0,255,0);
 	mp.setKdPhong(0,255,0);
 	mp.setKsPhong(0,120,0);
-	//shared_ptr<figura> derecha = make_shared<plano>(plano(newPoint(0.5,0,0), newDir(-1,0,0),IMAGE,"C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\aragon.ppm", mp));
-	//derecha->setScale(400);
     shared_ptr<figura> derecha = make_shared<plano>(plano(newPoint(0.5,0,0), newDir(-1,0,0),mp));
-	//derecha->setScale(2.5);
     shared_ptr<figura> ESFERArefraccion = make_shared<esfera>(newPoint(0,0.35,0.5), 0.15, refraccion);
     shared_ptr<figura> ESFERAreflexion = make_shared<esfera>(newPoint(0.3,-0.3,0.9), 0.2, reflexion);
 	shared_ptr<figura> ESFERAreflexion2 = make_shared<esfera>(newPoint(0.3,-0.1,0.9), 0.2, reflexion);
@@ -88,7 +118,7 @@ list<shared_ptr<figura>> setUpScene(){
 	PR.setKdPhong(0,255,255);
 	PR.setKsPhong(0,255,255);
 	PR.setAlfa(0);
-	shared_ptr<figura> ESFERAphong = make_shared<esfera>(newPoint(0,-0.35,0.7), 0.15, refraccion);
+	shared_ptr<figura> ESFERAphong = make_shared<esfera>(newPoint(0,-0.4,0.7), 0.1, refraccion);
 	mp.setKdPhong(0,0,255);
 	mp.setKsPhong(0,0,120);
 	mp.setAlfa(4);
@@ -98,26 +128,23 @@ list<shared_ptr<figura>> setUpScene(){
 	shared_ptr<figura> ESFERAphong5 = make_shared<esfera>(newPoint(-0.15,0.275,0.75), 0.1, mp);
 	shared_ptr<figura> ESFERAphong6 = make_shared<esfera>(newPoint(0.15,0.275,0.85), 0.1, mp);
 
-
-    //elementos.push_back(puntoLuz);
-	//elementos.push_back(puntoLuz2);
+    elementos.push_back(puntoLuz);
 	elementos.push_back(fondo);
     elementos.push_back(suelo);
     elementos.push_back(techo);
     elementos.push_back(izquierda);
     elementos.push_back(derecha);
     //elementos.push_back(ESFERArefraccion);
-	/*elementos.push_back(ESFERAreflexion);
+	elementos.push_back(ESFERAreflexion);
 	elementos.push_back(ESFERAreflexion2);
 	elementos.push_back(ESFERAreflexion3);
-	elementos.push_back(ESFERAreflexion4);*/
+	elementos.push_back(ESFERAreflexion4);
 	elementos.push_back(ESFERAphong);
-	/*elementos.push_back(ESFERAphong2);
+	elementos.push_back(ESFERAphong2);
 	elementos.push_back(ESFERAphong3);
 	elementos.push_back(ESFERAphong4);
 	elementos.push_back(ESFERAphong5);
-	elementos.push_back(ESFERAphong6);*/
-
+	elementos.push_back(ESFERAphong6);
 
     double brdfValues2[] = {0.9,0.0,0.0};
     materialProperties limit = materialProperties(false, eventos, brdfValues2,0);
@@ -133,8 +160,73 @@ list<shared_ptr<figura>> setUpScene(){
     return elementos;
 }
 
+list<shared_ptr<figura>> scene_1(){
+
+    double reflection[] =   {0,0.0,0.90};
+    double refraction[] =   {0,0.90,0.0};
+    double phong[] =        {0.90,0.0,0.0};
+    event_enum eventos[] =  {PHONG, REFRACTION, REFLEXION };
+
+
+    list<shared_ptr<figura>> elementos = set_room();
+
+    materialProperties mirror = materialProperties(false, eventos, reflection);
+    materialProperties glass = materialProperties(false, eventos, refraction, 1.5);
+    glass.setKd(255,255,255);
+    mirror.setKs(255,255,255);
+
+    shared_ptr<figura> sphere1 = make_shared<esfera>(newPoint(0.25,-0.15, 0.75), 0.15, glass);
+    shared_ptr<figura> sphere2 = make_shared<esfera>(newPoint(-0.25,-0.25,0.75), 0.15, mirror);
+
+    elementos.push_back(sphere1);
+    elementos.push_back(sphere2);
+
+    return elementos;
+}
+
+list<shared_ptr<figura>> scene_4(){
+
+    double reflection[] =   {0,0.0,0.90};
+    double refraction[] =   {0,0.90,0.0};
+    double phong[] =        {0.90,0.0,0.0};
+    event_enum eventos[] =  {PHONG, REFRACTION, REFLEXION };
+
+    materialProperties orange = materialProperties(false, eventos, phong);
+    orange.setKdPhong(216,21,21);
+    orange.setKsPhong(216,21,21);
+
+    list<shared_ptr<figura>> elementos = set_room();
+
+    materialProperties mirror = materialProperties(false, eventos, reflection);
+    materialProperties glass = materialProperties(false, eventos, refraction, 1.5);
+    glass.setKd(255,255,255);
+    mirror.setKs(255,255,255);
+
+    shared_ptr<figura> sphere1 = make_shared<esfera>(newPoint(0,-0.1, 0.7), 0.3, glass);
+    shared_ptr<figura> sphere2 = make_shared<esfera>(newPoint(0,-0.1, 0.7), 0.15, orange);
+
+    elementos.push_back(sphere1);
+    elementos.push_back(sphere2);
+
+    return elementos;
+}
+
+// Devuelve diversas escenas según id
+list<shared_ptr<figura>> setUpScene(int id){
+	if(id  == 1){
+		return scene_1();
+	}
+	if(id == 4){
+		return scene_4();
+	}
+	else{
+		return setUpSceneCool();
+	}
+}
+
 int completadas = 0;
 
+// Genera un fragmento de la imagen de la escena
 void generateScene( monteCarlo mc, const list<shared_ptr<figura>> &e,
 					const string &fOut, const int &hMin, const int &hMax,
 					const int &w, const int &h, const int &luzPuntual){
@@ -154,7 +246,17 @@ void generateScene( monteCarlo mc, const list<shared_ptr<figura>> &e,
 	flujoOut.close();
 }
 
-int main(){
+int main(int argc, char *argv[]){
+	// Comrpobación del número de argumentos
+	if(argc != 5){
+		cout << "Parametros: rayos por pixel, anchura imnagen, altura imagen, nombre imagen" << endl;
+		return 0;
+	}
+	// Si se usa el algoritmo de photon mapping o path tracer
+	bool photOn = false;
+	// Identificador de la escena, de 1 a 5
+	int nScene = 4;
+	
 	/*
      * camara       x   y   z
      *      o   =   0   0   0
@@ -169,62 +271,57 @@ int main(){
     dir alturaPlano = newDir(0,1,0);
     dir distanciaPlano = newDir(0,0,1);
 
-
 	c =  newCamara(origenCamara, alturaPlano, anchuraPlano, distanciaPlano);
-    double rpp = 0, w = 0, h = 0;
+	
+	// Parámetros path tracer
+	// Solo se soporta 1x1 de aspect ratio
+    double rpp = atoi(argv[1]), w = max(atoi(argv[2]), atoi(argv[3])), h = w;
 	int maxRays, maxPhotonsGlobal, maxPhotonsCaustics, photonsPerRay;
-    string fOut = "";
 
-    /*cout << "Introduce la altura de la imagen deseada:" << endl;
-     *
-    cin >> h;
-
-    cout << "Introduce la anchura de la imagen deseada:" << endl;
-    cin >> w;
-
-	cout << "Introduce el numero de rayos por pixel deseado:" << endl;
-    cin >> rpp;
-
-    cout << "Introduce el nombre del fichero de salida:" << endl;
-    cin >> fOut;
-    */
+	// Si la escena contiene luces puntuales
 	bool luzPuntual = false;
-	bool photOn = false;
+	
+	// Photon maps para iluminación global y cáusticas
 	photonMap pm, pmC;
 	
-	double gamma = 0.10;
-	maxRays = 100000;
-	maxPhotonsGlobal = 1000;
-	maxPhotonsCaustics = 1000;
-	photonsPerRay =  30;
-    h = 100;
-    w = 100;
-    rpp = 50;
+	double gamma = 0.3;
+	
+	// Parámetros para el photon map
+	maxRays = 1000000;
+	maxPhotonsGlobal = 50000;
+	maxPhotonsCaustics = 50000;
+	photonsPerRay =  25;
+
     int threads = 4;
     if (threads > h || threads > w){
         cerr << "Numero de threads incompatible con la resolucion de la imagen" << endl;
         exit(5);
     }
-    fOut = "test";
-    string ruta = "C:\\Users\\BlueSac\\Desktop\\Tools\\WorspaceCodelite\\Practica3IG\\";
 
-	list<shared_ptr<figura>> e = setUpScene();
+    string ruta = argv[4];
+
+	// Preparamos la escena
+	list<shared_ptr<figura>> e = setUpScene(nScene);
 	
 	thread th[threads];
 	int hMin = 0, hMax = - 1 + h/threads;
 	
 	monteCarlo mc(c,h,w,rpp, gamma);
 	photonMapper aux(maxRays, maxPhotonsGlobal, maxPhotonsCaustics);
+	// Si se usa photon mapping se genera el mapa de photones para iluminación global
+	// y de cáusticas
 	if(photOn){
 		aux.generatePhotonMap(pm, pmC, e,mc.getLuces(e));
 		cout << "Se ha generado el photon map"<< endl;
 		cout << "Photones de iluminacion global: " << pm.size() << endl;
 		cout << "Photones de cáusticas: " << pmC.size() << endl;
 	}
-	
+	// Dividimos el trabajo
 	for(int i = 0; i < threads;++i){
 		camara c2 = c;
+		// Si es path tracing
 		mc = monteCarlo(c2,h,w,rpp, gamma);
+		// Si es photon mapping
 		if(photOn){
 			mc = monteCarlo(c2,h,w,rpp, gamma, photonMap(pm.generateTreeAux()), 
 					photonMap(pmC.generateTreeAux()), photonsPerRay);
@@ -235,20 +332,21 @@ int main(){
 		hMax += h/threads;
 		cout << "Se ha creado el thread " << i << endl;
 	}
+	// Vaciamos los photon maps auxiliares
 	pm.clear();
 	pmC.clear();
 	for(int i = 0; i < threads;++i){th[i].join();}
 	
-	fOut = fOut + ".ppm";
-    fstream flujoOut;
-    string fOutAux = ruta + fOut;
+	fstream flujoOut;
+    string fOutAux = ruta;
     flujoOut.open((fOutAux).c_str(), ios::out);
     flujoOut    <<      "P3"     << endl
                 <<   "#MAX="  << MAX << endl
-                <<  "# " << fOut << endl
+                <<  "# " << fOutAux << endl
                 << w << " " << h << endl
                 <<      CR      << endl;
-				
+	
+	// Juntamos los fragmentos de la imagen
 	for(int i = 0; i < threads;++i){
 		string file = ruta+to_string(i);
 		fstream flujoAux;
